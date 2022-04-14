b04c80653c7ca33d5b59a469c176cd1d6f42a376
zempkey <zempkey@email.arizona.edu>
2022-04-14 16:11:19 -0700

    Added a file handler. Now can pass "lines" list to parser.

--- language_translator.py
+++ language_translator.py
@@ -5,12 +5,25 @@ CSc 372 Proj 2
 """
 import re
 
+
 def file_handler(file_name):
-    pass
+    """
+    Separates the file into individual lines. Newlines are maintained.
+    :param file_name: Name of the file
+    :return: A list of strings
+    """
+    try:
+        with open(file_name) as file:
+            lines = file.readlines()
+    except FileNotFoundError:
+        print("File not found. Ensure that the file is in the same directory and that the name was typed without"
+              " errors.")
+    return lines
 
 
 def main():
-    file_name = input()
+    # file_name = input()
+    file_name = "file_test.txt"
     lines = file_handler(file_name)
 
 
