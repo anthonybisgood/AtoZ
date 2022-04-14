da54a5d5a7a7b71da8ea15dd51fd644d5a507f9f
zempkey <zempkey@email.arizona.edu>
2022-04-14 15:54:57 -0700

    Bare bones

---
+++ language_translator.py
@@ -0,0 +1,18 @@
+"""
+language_translator.py
+Zach Empkey and Anthony Bisgood
+CSc 372 Proj 2
+"""
+import re
+
+def file_handler(file_name):
+    pass
+
+
+def main():
+    file_name = input()
+    lines = file_handler(file_name)
+
+
+if __name__ == "__main__":
+    main()
